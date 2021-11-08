9a139042d774ec86e4d138742e49a5c98ca7290a
Luan Fernandes <luangabriel70@gmail.com>
2021-11-08 09:09:06 -0300

    add visited states to search problems

--- chapter2/generic_search.py
+++ chapter2/generic_search.py
@@ -172,8 +172,11 @@ def dfs(
 
 
 def bfs(
-    initial: T, goal_test: Callable[[T], bool], successors: Callable[[T], List[T]]
-) -> Optional[Node[T]]:
+    initial: T,
+    goal_test: Callable[[T], bool],
+    successors: Callable[[T], List[T]],
+    return_visited_states: bool = False,
+) -> Union[Optional[Node[T]], Tuple[Optional[Node[T]], int]]:
     """
     Generalized Breadth-First Search Algorithm.
     """
@@ -182,14 +185,19 @@ def bfs(
     frontier.push(Node(initial, None))
     # explored is where we've been
     explored: Set[T] = {initial}
+    visited_states: int = 0
 
     # keep going while there is more to explore
     while not frontier.empty:
         current_node: Node[T] = frontier.pop()  # takes from the left!
+        visited_states += 1  # add one more visited state
         current_state: T = current_node.state
         # if we found the goal, we're done
         if goal_test(current_state):
-            return current_node
+            if return_visited_states:
+                return (current_node, visited_states)
+            else:
+                return current_node
         # check where we can go next and haven't explored
         for child in successors(current_state):
             if child in explored:  # skip children we already explored
@@ -204,20 +212,26 @@ def astar(
     goal_test: Callable[[T], bool],
     successors: Callable[[T], List[T]],
     heuristic: Callable[[T], float],
-) -> Optional[Node[T]]:
+    return_visited_states: bool = False,
+) -> Union[Optional[Node[T]], Tuple[Optional[Node[T]], int]]:
     # frontier is where we've yet to go
     frontier: PriorityQueue[Node[T]] = PriorityQueue()
     frontier.push(Node(initial, None, 0.0, heuristic(initial)))
     # explored is where we've been
     explored: Dict[T, float] = {initial: 0.0}
+    visited_states: int = 0
 
     # keep going while there is more to explore
     while not frontier.empty:
         current_node: Node[T] = frontier.pop()
+        visited_states += 1  # add one more visited state
         current_state: T = current_node.state
         # if we found the goal, we're done
         if goal_test(current_state):
-            return current_node
+            if return_visited_states:
+                return (current_node, visited_states)
+            else:
+                return current_node
         # check where we can go next and haven't explored
         for child in successors(current_state):
             new_cost: float = (
